d70843cf2e36397fc22a8758800085a64decccdb
kylecorry31 <kylecorry31@gmail.com>
2016-01-21 17:04:56 -0500

    Add triangles to corners for cool geometric effect

--- image_artist.py
+++ image_artist.py
@@ -4,10 +4,31 @@ import PIL.ImageDraw
 import matplotlib.pyplot as plt
     
 def convert_to_grayscale(image):
-    return image.convert('LA')
+    """ Converts a PIL.Image to grayscale
+
+    image: A PIL.Image
+
+    Returns a PIL.Image in grayscale.
+    """
+    image = image.convert('LA')
+    return image.convert("RGBA")
     
 def add_logo(image, logo, corner):
+    """ Adds a logo to a PIL.Image
+
+    image: A PIL.Image which will receive a logo
+    logo: A PIL.Image which will be used as a logo
+    corner: The number corresponding to the corner that the logo will be placed
+            Bottom left: 0
+            Bottom right: 1
+            Top left: 2
+            Top right: 3
+
+    Returns a PIL.Image with the logo applied to the correct corner.
+    """
     (width, height) = logo.size
+
+    # Resize the logo
     logo_max = max([height, width])
     logo_aspect_ratio = height / float(width)
     if logo_max == height:
@@ -18,6 +39,8 @@ def add_logo(image, logo, corner):
         width = int(0.22 * image.size[0])
         height = int(width *  logo_aspect_ratio)
         logo = logo.resize((width, height))
+        
+    # Calculate logo padding / positioning based on corner
     btm_left = 0
     btm_right = 1
     top_left = 2
@@ -33,19 +56,36 @@ def add_logo(image, logo, corner):
     image.paste(logo, padding, mask=logo)
     return image
     
-def add_triangle_corners(original_image, color, wide):
+def add_triangle_corners(original_image, wide, color):
+    """ Adds triangles to the corners of a PIL.Image. Should be used after a border is applied
+
+    original_image: A PIL.Image which will receive the triangle corners
+    wide: The width and height of the triangle
+    color: The color of the triangle
+
+    Returns a PIL.Image with triangles in the corners
+    """
     draw = PIL.ImageDraw.Draw(original_image)
-    draw.polygon([(0, 0), (0, wide), (wide, 0)], fill=(0, 0, 0))
+    draw.polygon([(0, 0), (0, wide), (wide, 0)], fill=color) # top left
+    draw.polygon([(original_image.size[0], 0),
+                  (original_image.size[0], wide),
+                  (original_image.size[0] - wide, 0)], fill=color) # top right
+    draw.polygon([(0, original_image.size[1]),
+                  (0, original_image.size[1] - wide),
+                  (wide, original_image.size[1])], fill=color) # bottom left
+    draw.polygon([(original_image.size[0], original_image.size[1]),
+                  (original_image.size[0], original_image.size[1] - wide),
+                  (original_image.size[0] - wide, original_image.size[1])], fill=color) # bottom right
     return original_image    
     
 def frame_image(original_image, color, wide):
     """ Frames a PIL.Image
     
-    original_image must be a PIL.Image
+    original_image: A PIL.Image which will receive a frame
     color: The color of the border as a 3-tuple of RGB color (R, G, B)
     wide: The width in pixels of the border.
     
-    Returns a new PIL.Image with borders.
+    Returns a PIL.Image with borders.
     """
     width, height = original_image.size
     border_mask = PIL.Image.new('RGBA', (width, height), (127,0,127,0))
@@ -67,7 +107,6 @@ def manipulate_image(current_image, current_filename, corner, gray):
     logo = PIL.Image.open(os.path.join(os.getcwd(), 'logo.png'))
     if gray:
         current_image = convert_to_grayscale(current_image)
-    current_image = current_image.convert("RGBA")
     current_image = add_logo(current_image, logo, corner)
     current_image = add_border(current_image, 0.05 * current_image.size[0], (255, 255, 255))
     current_image = add_triangle_corners(current_image, 0.05 * current_image.size[0], (0, 0, 0))
@@ -85,7 +124,7 @@ def get_images(directory=None):
     
     if directory == None:
         directory = os.path.join(os.getcwd(), "Images") # Use working directory if unspecified
-    image_list = [] # Initialize aggregaotrs
+    image_list = []
     file_list = []
     new_directory = os.path.join(os.getcwd(), 'modified')
     try:
@@ -104,6 +143,8 @@ def get_images(directory=None):
     return image_list, file_list
 
 def main():
+    """ The main function of the program. It is used to get the client's preferences and manipulate each image in the images directory.
+    """
     images = get_images()
     corner = int(raw_input("Location of the logo: bottom left (0), bottom right (1), top left (2), top right (3)\n"))
     gray = bool(int(raw_input("Make image grayscale? (1 = yes, 0 = no)\n")))
